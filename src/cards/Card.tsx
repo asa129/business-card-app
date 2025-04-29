28fdc94d566b9c8ca895b95ce7565283e985fe46
asa129 <asa.jad1290@gmail.com>
2025-04-29 10:33:51 +0900

    値がないときはloadingを表示する

--- src/cards/Card.tsx
+++ src/cards/Card.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { getUserById } from "../utils/supabaseFunctions";
 
 export const Card = function Card() {
   const { id } = useParams();
+  const [loadingFlag, setLoadingFlag] = useState(true);
+  const [user, setUser] = useState<any[]>();
+  const getData = async () => {
+    const data = await getUserById(id!);
+    setUser(data);
+    if (data.length !== 0) {
+      setLoadingFlag(false);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  if (loadingFlag) {
+    return <h1>loading…</h1>;
+  }
   return (
     <>
       <p>id: {id}</p>
+      {user?.map((user) => {
+        return (
+          <>
+            <p>名前:{user.name}</p>
+            <p>自己紹介:{user.description}</p>
+            <p>スキル:{user.user_skill[0].skills.name}</p>
+            <p>Github:{user.github_id}</p>
+            <p>Qiita:{user.qiita_id}</p>
+            <p>X:{user.x_id}</p>
+          </>
+        );
+      })}
     </>
   );
 };
