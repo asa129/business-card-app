cb1747cc4b6c70a2b7a8dfcb952a4123f61c13af
asa129 <asa.jad1290@gmail.com>
2025-05-04 20:50:36 +0900

    バリデーションを導入

--- src/cards/RegistCard.tsx
+++ src/cards/RegistCard.tsx
@@ -3,14 +3,26 @@ import {
   Button,
   Card,
   FormControl,
-  FormHelperText,
   FormLabel,
   Heading,
   Input,
   Select,
+  Textarea,
 } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 export const RegistCard = () => {
+  type Form = {
+    name: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit: SubmitHandler<Form> = (data) => console.log(data);
+  console.log(errors);
+
   return (
     <>
       <Box
@@ -35,42 +47,79 @@ export const RegistCard = () => {
           borderRadius="md"
           boxShadow="md"
         >
-          <Card boxShadow="none">
-            <FormControl mb={4}>
-              <FormLabel>好きな英単語</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl isRequired mb={4}>
-              <FormLabel>お名前</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl isRequired mb={4}>
-              <FormLabel>自己紹介</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl isRequired mb={4}>
-              <FormLabel>好きな技術</FormLabel>
-              <Select placeholder="Select country">
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
-              </Select>
-            </FormControl>
-            <FormControl mb={4}>
-              <FormLabel>GithubId</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl mb={4}>
-              <FormLabel>QiitaId</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl mb={4}>
-              <FormLabel>TwitterId</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <Button mt={4} colorScheme="teal" type="submit">
-              登録
-            </Button>
-          </Card>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Card boxShadow="none">
+              <FormControl mb={4}>
+                <FormLabel>好きな英単語</FormLabel>
+                <Input
+                  {...register("id", {
+                    pattern: /^[A-Za-z]*$/,
+                  })}
+                  type="text"
+                />
+                {errors.id?.type === "pattern" && (
+                  <p style={{ color: "red" }}>
+                    英語のみ入力可能です
+                    <br />
+                    ※スペースは入れないでください
+                  </p>
+                )}
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>
+                  お名前 <span style={{ color: "red" }}>*</span>
+                </FormLabel>
+                <Input {...register("name", { required: true })} type="text" />
+                {errors.name?.type === "required" && (
+                  <p style={{ color: "red" }}>お名前は必須です</p>
+                )}
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>
+                  自己紹介 <span style={{ color: "red" }}>*</span>
+                </FormLabel>
+                <Textarea
+                  resize="vertical"
+                  {...register("selfIntroduction", { required: true })}
+                  placeholder="<h1>HTMLタグも使えます</h1>"
+                />
+                {errors.selfIntroduction?.type === "required" && (
+                  <p style={{ color: "red" }}>自己紹介は必須です</p>
+                )}
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>
+                  好きな技術 <span style={{ color: "red" }}>*</span>
+                </FormLabel>
+                <Select
+                  {...register("favoriteTechnique", { required: true })}
+                  placeholder="選んでね"
+                >
+                  <option>Github</option>
+                  <option>Qiita</option>
+                  <option>X</option>
+                </Select>
+                {errors.favoriteTechnique?.type === "required" && (
+                  <p style={{ color: "red" }}>好きな技術は必須です</p>
+                )}
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>GithubId</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>QiitaId</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel>TwitterId</FormLabel>
+                <Input type="text" />
+              </FormControl>
+              <Button mt={4} colorScheme="teal" type="submit">
+                登録
+              </Button>
+            </Card>
+          </form>
         </Box>
       </Box>
     </>
