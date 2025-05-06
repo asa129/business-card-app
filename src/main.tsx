14867feb5f6a6beca4e901a61d2a1f6bc3f6d4c2
asa129 <asa.jad1290@gmail.com>
2025-05-06 21:58:49 +0900

    不要なimport削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { UserCard } from "./cards/UserCard.tsx";
