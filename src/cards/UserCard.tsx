cb16c061959bab6a8947c9f52aea66a462a1cf61
asa129 <asa.jad1290@gmail.com>
2025-05-06 22:17:26 +0900

    戻るボタンを追加

--- src/cards/UserCard.tsx
+++ src/cards/UserCard.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { getUserById } from "../utils/supabaseFunctions";
 import { User } from "../domain/user";
-import { Box, Card, CardBody, Heading } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, Heading } from "@chakra-ui/react";
 import { IoLogoGithub } from "react-icons/io5";
 import { SiQiita } from "react-icons/si";
 import { RiTwitterXFill } from "react-icons/ri";
@@ -11,6 +11,7 @@ export const UserCard = () => {
   const { id } = useParams();
   const [loadingFlag, setLoadingFlag] = useState(true);
   const [user, setUser] = useState<User[]>();
+  const navigate = useNavigate();
   const getData = async () => {
     const data = await getUserById(id!);
     setUser(data);
@@ -28,6 +29,7 @@ export const UserCard = () => {
     <Box
       height="100vh"
       display="flex"
+      flexDirection="column"
       alignItems="center"
       justifyContent="center"
       bg="#C4F1F9"
@@ -79,6 +81,11 @@ export const UserCard = () => {
             </Card>
           );
         })}
+      </Box>
+      <Box width="sm" p={4} textAlign="center">
+        <Button w="100%" mt={4} colorScheme="teal" onClick={() => navigate(-1)}>
+          戻る
+        </Button>
       </Box>
     </Box>
   );
