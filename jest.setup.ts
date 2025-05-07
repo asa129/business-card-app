7582d33025a878d31e210bf2c0279a269a93acfa
asa129 <asa.jad1290@gmail.com>
2025-05-08 00:07:00 +0900

    テストでreact-routerが使えるように設定

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,9 @@ import "@testing-library/jest-dom";
 import { config } from "dotenv";
 
 config();
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+global.TextEncoder = require("util").TextEncoder;
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+global.TextDecoder = require("util").TextDecoder;
